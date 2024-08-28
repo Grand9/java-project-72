247ce36955b9d709c19730acee1adeae4547fe7e
Arlan <Arlan110022@Gmail.com>
2024-08-28 13:22:36 +0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("io.javalin:javalin:6.2.0")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("gg.jte:jte:3.1.12")
+    implementation("io.javalin:javalin-rendering:6.2.0")
 
     implementation("com.h2database:h2:2.2.220")
     implementation("com.zaxxer:HikariCP:5.0.1")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,26 +1,27 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
 
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
     public static Javalin getApp() {
-        return Javalin.create(config -> {
+        var app = Javalin.create(config -> config.fileRenderer(new JavalinJte()));
+
+        app.get("/", ctx -> {
+            LOGGER.info("Request received at /");
+            ctx.result("Hello World");
         });
+
+        return app;
     }
 
     public static void main(String[] args) {
         Javalin app = getApp();
-
-        app.get("/", ctx -> {
-            logger.info("Request received at /");
-            ctx.result("Hello World");
-        });
-
         app.start(7070);
     }
 }
