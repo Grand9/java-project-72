bd79514a9234df72adfac597fce41d1c75532f2f
Arlan <Arlan110022@Gmail.com>
2024-08-29 12:00:18 +0500

    update

--- app/src/main/java/hexlet/code/config/DatabaseConfig.java
+++ app/src/main/java/hexlet/code/config/DatabaseConfig.java
@@ -9,13 +9,17 @@ public class DatabaseConfig {
 
     public static DataSource createDataSource() {
         String jdbcUrl = System.getenv("JDBC_DATABASE_URL");
-
-        if (jdbcUrl == null) {
-            throw new IllegalStateException("JDBC_DATABASE_URL environment variable is not set");
-        }
-
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(jdbcUrl);
+
+        if (jdbcUrl == null || jdbcUrl.isEmpty()) {
+            config.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1");
+            config.setDriverClassName("org.h2.Driver");
+            config.setUsername("sa");
+            config.setPassword("");
+        } else {
+            config.setJdbcUrl(jdbcUrl);
+            config.setDriverClassName("org.postgresql.Driver");
+        }
 
         return new HikariDataSource(config);
     }
