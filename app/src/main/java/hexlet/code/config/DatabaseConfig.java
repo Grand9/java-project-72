8c027701f3d6ac9383a0b5d33b5482565ef4cc1b
Arlan <Arlan110022@Gmail.com>
2024-08-29 16:24:52 +0500

    update

--- app/src/main/java/hexlet/code/config/DatabaseConfig.java
+++ app/src/main/java/hexlet/code/config/DatabaseConfig.java
@@ -8,9 +8,9 @@ import javax.sql.DataSource;
 public class DatabaseConfig {
 
     public static DataSource createDataSource() {
-        String jdbcUrl = "jdbc:postgresql://dpg-cr82he3tq21c739iksv0-a:5432/database_hexlet" +
-                "?user=grand_hexlet" +
-                "&password=tnkB7aEoSf1TcQuSKCfokHjYyVjGRFn1";
+        String jdbcUrl = "postgresql://grand_hexlet:tnkB7aEoSf1TcQuSKCfokHjYyVjGRFn1@"
+                + "dpg-cr82he3tq21c739iksv0-a/"
+                + "database_hexlet";
         HikariConfig config = new HikariConfig();
 
         config.setJdbcUrl(jdbcUrl);
