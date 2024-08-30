2db1e914edd1c699a2a982bc5e8c7b0105cdfeb4
Arlan <Arlan110022@Gmail.com>
2024-08-30 17:09:39 +0500

    update

--- app/src/main/java/hexlet/code/config/DatabaseConfig.java
+++ app/src/main/java/hexlet/code/config/DatabaseConfig.java
@@ -9,11 +9,9 @@ public class DatabaseConfig {
 
     public static DataSource createDataSource() {
         String jdbcUrl = System.getenv("JDBC_DATABASE_URL");
-        
         if (jdbcUrl == null || jdbcUrl.isEmpty()) {
-            throw new IllegalStateException("JDBC_DATABASE_URL environment variable is not set");
+            throw new IllegalStateException("JDBC_DATABASE_URL environment variable not set");
         }
-
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(jdbcUrl);
         config.setDriverClassName("org.postgresql.Driver");
