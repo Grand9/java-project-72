56cdbded7d49ab8071daa14441e95f00a9042f6d
Arlan <Arlan110022@Gmail.com>
2024-08-31 19:30:15 +0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,22 +14,22 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-
-    implementation("io.javalin:javalin:6.2.0")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("gg.jte:jte:3.1.12")
-    implementation("io.javalin:javalin-rendering:6.2.0")
-
-    implementation("com.h2database:h2:2.2.220")
-    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("org.postgresql:postgresql:42.7.2")
-    implementation("net.datafaker:datafaker:2.0.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("org.apache.commons:commons-text:1.11.0")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("org.apache.commons:commons-lang3:3.13.0")
+
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 application {
